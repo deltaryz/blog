5a0c633b79cc86783906c257b97ad7568bf46651
deltaryz <wincam97@gmail.com>
2023-07-11 00:24:32 -0500

    Force usage of updated node

--- update.sh
+++ update.sh
@@ -5,6 +5,9 @@
 
 # For the sake of OS-agnosticity, this script assumes node and npm are already present on the system.
 
+node_path=$(which node)
+npm_path=$(which npm)
+
 # Check if n is installed
 if ! command -v n >/dev/null 2>&1; then
     echo "n is not installed. Installing n..."
@@ -18,31 +21,30 @@ if [ "$(node --version)" != "$latest_version" ]; then
     echo "There is a node.js update ($latest_version) available. Updating node and npm..."
     n latest
 
-    # make sure the new executable is present in PATH
-    export PATH=/usr/local/bin:$PATH
-    echo "Checking updated node version: $(node --version)"
+    node_path=$(n which latest)
+    node_dir=$(echo "$node_path" | sed 's/\/node$//')
+    npm_path="${node_dir}/npm"
 
-    npm install -g npm@latest
+    # make sure the new executable is present in PATH
+    echo "Checking updated node version: "
+    eval "${node_path} --version"
+
+    # update npm
+    eval "${npm_path} install -g npm@latest"
 fi
 
 # Check if TypeScript is installed
 if ! command -v tsc >/dev/null 2>&1; then
     echo "TypeScript is not installed. Installing TypeScript..."
-    npm install -g typescript
-fi
-
-# Check if ts-node is installed
-if ! command -v ts-node >/dev/null 2>&1; then
-    echo "ts-node is not installed. Installing ts-node..."
-    npm install -g ts-node
+    eval "${npm_path} install -g typescript"
 fi
 
 # Check if `npm install .` needs to be run
 if [ ! -d "node_modules" ]; then
     echo "Dependencies not installed. Running 'npm install .'"
-    npm install .
+    eval "${npm_path} install ."
 fi
 
-echo "TypeScript, ts-node, and project dependencies are installed."
+echo "TypeScript and project dependencies are installed."
 
-ts-node makejson.ts
+tsc makejson.ts && eval "${node_path} makejson.js"
