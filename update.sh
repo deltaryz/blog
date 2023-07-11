142d86f549560ced19012dccd50943d938f33f38
deltaryz <wincam97@gmail.com>
2023-07-11 01:05:01 -0500

    dont bother updating node at runtime

--- update.sh
+++ update.sh
@@ -5,48 +5,22 @@
 
 # For the sake of OS-agnosticity, this script assumes node and npm are already present on the system.
 
-node_path=$(which node)
-npm_path=$(which npm)
-
-# Check if n is installed
-if ! command -v n >/dev/null 2>&1; then
-    echo "n is not installed. Installing n..."
-    npm install -g n
-fi
-
-# Check if the latest Node.js version is installed
-latest_version=$(n --latest)
-if [ "$(node --version)" != "$latest_version" ]; then
-    # Install the latest Node.js version
-    echo "There is a node.js update ($latest_version) available. Updating node and npm..."
-    n latest
-
-    node_path=$(n which latest)
-    node_dir=$(echo "$node_path" | sed 's/\/node$//')
-    npm_path="${node_dir}/npm"
-
-    n doctor
-
-    # make sure the new executable is present in PATH
-    echo "Checking updated node version: "
-    ${node_path} --version
-
-    # update npm
-    ${npm_path} install -g npm@latest
-fi
+echo "Checking for node and npm..."
+which node
+which npm
 
 # Check if TypeScript is installed
 if ! command -v tsc >/dev/null 2>&1; then
     echo "TypeScript is not installed. Installing TypeScript..."
-    ${npm_path} install -g typescript
+    npm install -g typescript
 fi
 
 # Check if `npm install .` needs to be run
 if [ ! -d "node_modules" ]; then
     echo "Dependencies not installed. Running 'npm install .'"
-    ${npm_path} install .
+    npm install .
 fi
 
 echo "TypeScript and project dependencies are installed."
 
-tsc makejson.ts && eval "${node_path} makejson.js"
+tsc makejson.ts && node makejson.js
