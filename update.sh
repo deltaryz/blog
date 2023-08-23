8bd840e2a912dbc40fc3aa9d5e207067c02d1185
deltaryz <wincam97@gmail.com>
2023-08-23 07:28:04 -0500

    Clean up json creation, use npm script

--- update.sh
+++ update.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-# This script will run `makejson.ts`
+# In a development environment, use `npm run-script makeJson` instead
+
+# This script is for my Caddy webserver
 # Caddy will be configured to run this automatically whenever the site updates
 
 # For the sake of OS-agnosticity, this script assumes node and npm are already present on the system.
 
 # Check if TypeScript is installed
-if ! command -v tsc >/dev/null 2>&1; then
-    echo "TypeScript is not installed. Installing TypeScript..."
-    npm install -g typescript
+if ! command -v tsx >/dev/null 2>&1; then
+    echo "TypeScript or tsx is not installed. Installing TypeScript..."
+    npm install -g typescript tsx
 fi
 
 # Make sure all dependencies are present
@@ -17,7 +19,7 @@ npm install .
 echo "TypeScript and project dependencies are installed."
 
 # Generate the posts.json
-tsc makejson.ts && node makejson.js
+npm run-script makeJson
 
 # This makes sure caddy will still be able to git pull over it
 echo "Forcibly resetting repo in case npm fucks something up"
